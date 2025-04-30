9de94ebd45307c76cd217ea4a91558e7e1c65dc4
Algorithmicmasonry <emmanuelandalgorithms@gmail.com>
2025-04-30 14:10:57 +0100

    pushing ui changes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,14 @@
-import heroImg from './hero1.png'
-import provImg from "./prov_image.png"
-import userRegImg from './user_reg.png'
-import docRegImg from './doc_reg.png'
-import facilityRegImg from './facility_reg.png'
-import facilityRegImg2 from './facility_reg2.png'
-import loginImg from './login_image.png'
+import heroImg from "./hero1.png";
+import provImg from "./prov_image.png";
+import userRegImg from "./user_reg.png";
+import docRegImg from "./doc_reg.png";
+import facilityRegImg from "./facility_reg.png";
+import facilityRegImg2 from "./facility_reg2.png";
+import loginImg from "./login_image.png";
 
 const appName = "WellJourney";
 // List of medical specializations
- export const medicalSpecializations = [
+export const medicalSpecializations = [
   "Cardiology",
   "Dermatology",
   "Endocrinology",
@@ -23,9 +23,16 @@ const appName = "WellJourney";
   "Psychiatry",
   "Radiology",
   "Surgery",
-  "Urology",
+  "Urology"
 ] as const;
 
-
-
-export {appName, heroImg, provImg, userRegImg, docRegImg, facilityRegImg, facilityRegImg2, loginImg,}
+export {
+  appName,
+  heroImg,
+  provImg,
+  userRegImg,
+  docRegImg,
+  facilityRegImg,
+  facilityRegImg2,
+  loginImg
+};
